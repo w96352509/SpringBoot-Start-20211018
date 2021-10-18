dde734074f8e87f1144276e96048ed04db34bb08
vic <vichsu38@gmail.com>
2021-10-18 21:46:59 +0800

    spring

--- SpringBoot-Start-20211018/src/main/java/com/example/demo/controller/MessageController.java
+++ SpringBoot-Start-20211018/src/main/java/com/example/demo/controller/MessageController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,10 +12,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MessageController {
 
 	@GetMapping("/message")
-	@ResponseBody
-	public String message() {
-		
-		return "Hello message";
+	public String message(Model model) {
+		model.addAttribute("name" ,"<h1>John</h1>");
+		List<Integer> scores =Arrays.asList(100,40,70,55);
+		model.addAttribute("scores" ,scores);
+		return "message";
 	}
-  	
+
 }
