d932f8eb9de1c442cb26e8aa95522c208a127d3c
vic <vichsu38@gmail.com>
2021-10-18 20:10:53 +0800

    spring

---
+++ SpringBoot-Start-20211018/src/main/java/com/example/demo/SpringBootStart20211018Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStart20211018Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStart20211018Application.class, args);
+	}
+
+}
